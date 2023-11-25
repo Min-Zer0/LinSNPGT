8cd24e4da0d6b6b784de937ea55f1ba8ac8a24c9
Min Zhu <1185643615@qq.com>
2023-11-25 15:31:01 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 mkdir 01.Reference_Genome 02.Input_Fastq
-mv .sys/install.Java8.sh .sys/SNPGT-build.py .sys/SNPGT.py .sys/SNPGT.config ./
-chmod +x install.Java8.sh SNPGT-build.py SNPGT.py
+mv .sys/install.Java8.sh .sys/SNPGT-build.py .sys/SNPGT.py .sys/SNPGT.config .sys/VCF2Geno.py ./
+chmod +x install.Java8.sh SNPGT-build.py SNPGT.py VCF2Geno.py
 chmod +x .sys/picard.jar .sys/GenomeAnalysisTK.jar
 mv install.sh .sys/
 echo "----------------------------"
